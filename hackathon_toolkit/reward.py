f89d7fd8c435931e8bf6e6bd195fc4e619765e4e
baptxste <baptiste.chachura@me.com>
2025-03-04 14:44:56 +0100

    rewards

--- hackathon_toolkit/reward.py
+++ hackathon_toolkit/reward.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+def manhattan_distance(pos1, pos2):
+    """Calculate Manhattan distance between two positions"""
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+# ajouter un cas pour traier laction =-1 ( pour le cas ou  l'agent n'est plsu en jeu)
 def compute_reward(num_agents, old_positions, agent_positions, evacuated_agents, deactivated_agents, goal_area):
     rewards = np.zeros(num_agents)
+    # print( "OLD : ", old_positions)
+    # print("NEW : ",agent_positions)
+    # print("GOALS : ", goal_area)
+
+
 
     # Compute reward for each agent
     for i, (old_pos, new_pos) in enumerate(zip(old_positions, agent_positions)):
@@ -13,7 +23,18 @@ def compute_reward(num_agents, old_positions, agent_positions, evacuated_agents,
             rewards[i] = 1000.0
             evacuated_agents.add(i)
         else:
-            # Penalties for not finding the goal
-            rewards[i] = -0.1
+            # if the agent came closer of the goal their is a small  postive reward 
+            # if if move away small negative reward
+            
+
+            d_old = manhattan_distance(old_positions[i], goal_area[i])
+            d_new= manhattan_distance(agent_positions[i], goal_area[i])
+            if d_old < d_new : 
+                rewards[i] = 10
+            else : rewards[i] = -1
+            
+            
+            
+            
 
     return rewards, evacuated_agents
